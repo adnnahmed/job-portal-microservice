ef3104dbaceebe4a16c65244f620e0dfeb1c51f8
adnnahmed <adnnahmed@gmail.com>
2024-02-13 04:39:29 +0530

    OpenFeign Implemented

--- review/src/main/java/personal/project/review/services/implementations/ReviewServiceImplementation.java
+++ review/src/main/java/personal/project/review/services/implementations/ReviewServiceImplementation.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 import personal.project.review.clients.CompanyClient;
 import personal.project.review.exceptions.ResourceUnavailableException;
 import personal.project.review.mapper.CompanyToReviewMapper;
